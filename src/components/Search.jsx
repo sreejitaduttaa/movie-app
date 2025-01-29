67710bc758e4cc678c1fa832ed1060a7b6701d4d
sreejitaduttaa <sreejitadutta23@gmail.com>
2025-01-29 20:55:15 +0530

    search added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 
 const Search = ({searchTerm, setSearchTerm}) => {
 
-    
-
   return (
-    <div className='text-white text-3xl'>Search</div>
+    <div className='search'>
+        <div>
+            <img src='search.svg' alt='search'/>
+
+            <input
+                type='text'            
+                placeholder='Search through thousands of movies'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+        </div>
+    </div>
   )
 }
 
